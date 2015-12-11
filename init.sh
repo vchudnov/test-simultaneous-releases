1d95e76d41d773bfaa8e46cdf770a9d1480e1bf3
Victor Chudnovsky <victor.chudnovsky@gmail.com>
2015-12-11 11:31:07 -0800

    Fix syntax error; make executable.

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ git submodule update --recursive
 
 pushd submodules/test-branched-releases
 
-# "git worktree" is the official replacemen of the previous,
+# "git worktree" is the official replacement of the previous,
 # community-contributed git-new-workdir.
 git worktree add ../tbs-1.1 release_1.1
 git worktree add ../tbs-2.0 release_2.0
