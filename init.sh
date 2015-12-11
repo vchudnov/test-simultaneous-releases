2d3f48bcf14605a278fd1d91f9b058fa628d97e5
Victor Chudnovsky <victor.chudnovsky@gmail.com>
2015-12-11 11:24:42 -0800

    Add script to set up the one submodule at different versions simulaenously.

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE="$(git rev-parse --show-toplevel)"
+
+pushd "$BASE"
+
+git submodule init
+git submodule update --recursive
+
+pushd submodules/test-branched-releases
+
+# "git worktree" is the official replacemen of the previous,
+# community-contributed git-new-workdir.
+git worktree add ../tbs-1.1 release_1.1
+git worktree add ../tbs-2.0 release_2.0
+
+popd
